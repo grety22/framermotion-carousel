e02ca70fc702e8c77152dd0033f9485b3f3364c7
Gretel Martinez <23240307+grety22@users.noreply.github.com>
2023-08-22 21:22:43 -0400

    framer motion carousel

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Framer Motion Carousel',
+  description: 'Framer Motion, Tailwind CSS, and Next.js Carousel',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-indigo-500 to-90% drop-shadow-xl">{children}</body>
+    </html>
+  )
+}
